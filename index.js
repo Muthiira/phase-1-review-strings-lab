46cb110e46867e7e86ca40d9ea56f4226675672b
Muthiira <calebmuchiri48@gmail.com>
2022-08-11 23:03:54 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = "Caleb";
+let welcomeMessage = "Welcome to Flatbook, Caleb!";
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, CALEB!";
+let shortGreeting = "Welcome, C!"
+
